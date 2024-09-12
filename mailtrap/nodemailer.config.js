ac3e2015204b6a7ae629f309a0e78cbe21bcc073
Baasir-stack <baasir121121@gmail.com>
2024-09-12 08:17:12 +0500

    Initial commit

---
+++ mailtrap/nodemailer.config.js
@@ -0,0 +1,17 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER, // Your Gmail address
+    pass: process.env.GMAIL_PASS, // Your Gmail password or App password
+  },
+});
+
+export const sender = {
+  email: process.env.GMAIL_USER,
+  name: "Baasir",
+};
