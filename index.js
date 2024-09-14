9439f2e6907317add46ffbd8a4d83f210595c1eb
Baasir-stack <baasir121121@gmail.com>
2024-09-14 19:23:10 +0500

    solve cors issue (1)

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS Configuration for your frontend
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // Parse incoming JSON requests
 app.use(cookieParser()); // Parse incoming cookies
